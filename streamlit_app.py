7cfe291e5096454dd16f154fa1e6ea0a8dae0c2f
mralexnolla <116486961+mralexnolla@users.noreply.github.com>
2026-07-13 08:35:23 -0400

    Update streamlit_app.py
    modifying the success message text, to include the name of the customer

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if ingredients_list:
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('This is a success message!', icon="✅")
+        st.success('Your Smoothie is ordered, {name_on_order}!', icon="✅")
