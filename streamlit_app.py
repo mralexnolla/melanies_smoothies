9828374ad4d00c426090a4ea013b6b2a4ca602f8
mralexnolla <116486961+mralexnolla@users.noreply.github.com>
2026-07-15 07:16:36 -0400

    Update streamlit_app.py
    refactoring the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredient_string += fruit_chosen + ' '
       
-        # st.write(ingredient_string)
-        # st.subheader(fruit_chosen + ' Nutrition Information')
-        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
-        # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.write(ingredient_string)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
         
 
@@ -60,11 +60,11 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-if ingredients_list:
-  ingredients_string = ''
+# if ingredients_list:
+#   ingredients_string = ''
   
-  for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ' '
-    st.subheader(fruit_chosen + ' Nutrition Information')
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+#   for fruit_chosen in ingredients_list:
+#     ingredients_string += fruit_chosen + ' '
+#     st.subheader(fruit_chosen + ' Nutrition Information')
+#     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+#     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
