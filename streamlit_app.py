b49e9090f9ba364c2b53852b82fd8c77c42d0d2c
mralexnolla <116486961+mralexnolla@users.noreply.github.com>
2026-07-15 07:54:03 -0400

    Update streamlit_app.py
    commenting an output

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         ingredient_string += fruit_chosen + ' '
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
       
         # st.write(ingredient_string)
         st.subheader(fruit_chosen + ' Nutrition Information')
